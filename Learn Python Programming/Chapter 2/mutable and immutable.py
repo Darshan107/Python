ea00aa447792ca56926a5d15578e3c3c8ee1ffe0
Darshan107 <timilsinadarshan107@gmail.com>
2024-08-28 12:20:15 +0545

    mutable an immutable example with id() func

---
+++ Learn Python Programming/Chapter 2/mutable and immutable.py
@@ -0,0 +1,23 @@
+age = 42
+age = 43
+# these may seem like we can change integers but, 
+
+hello = 10
+print(id(hello))
+hello = 40
+print(id(hello))
+# both id's will be different proving that they are different object
+# what happens is that when we define name = value, a object (with its unique id, suitable type, and value given) is created and the name actually points to that object.
+# reassigning the name to something else creates a new object which the name points to.
+
+# here is an example of mutable data type
+class Person(object):
+    def __init__(self,age):
+        self.age = age
+fab = Person(age = 24)
+print(fab.age)
+print(f"{id(fab) = }, {id(fab.age) = }")
+fab.age = 40
+print(f"{id(fab) = }, {id(fab.age) = }")
+
+#fab being an object, is mutable and its id dont change while fab.age only changes its id
